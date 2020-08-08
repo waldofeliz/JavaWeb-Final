5e23035a514142ac984d8171c4dce5afd5507f1b
Waldo Feliz <wafeliz@humano.com.do>
2020-08-08 16:51:29 -0400

    Ajuste Source

--- sources/scripts/inicio.js
+++ sources/scripts/inicio.js
@@ -1,29 +1,29 @@
-$(function() {
+$(function () {
 
     //Componentes
     let body = document.getElementById('body')
 
     body.onload = () => {
         let current = JSON.parse(localStorage.getItem("usuarioSesion"))
-        console.log('Usuario',current)
-        if(current === null){
+        console.log('Usuario', current)
+        if (current === null) {
             window.location.href = './login.html'
         }
     }
 
-/* Optional: Add active class to the current button (highlight it) */
-var container = document.getElementById("btnContainer");
-console.log('Container',container)
-var btns = container.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+    /* Optional: Add active class to the current button (highlight it) */
+    var container = document.getElementById("btnContainer");
+    console.log('Container', container)
+    var btns = container.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
 
-rowImagenes()
+    rowImagenes()
 
 });
 
@@ -36,56 +36,58 @@ var i;
 
 // List View
 function listView() {
-  for (i = 0; i < elements.length; i++) {
-      console.log('Elements: ',elements[i].getElementsByTagName('img'))
-    elements[i].style.width = "100%";
-    elements[i].style.display = "flex";
-    elements[i].style.flexDirection = "column";
-    elements[i].style.justifyContent = "center";
-    elements[i].style.alignItems = "center";
-    elements[i].getElementsByTagName('img')[0].style.width = "400px";
-    elements[i].getElementsByTagName('img')[0].style.height = "300px";
-  }
+    for (i = 0; i < elements.length; i++) {
+        console.log('Elements: ', elements[i].getElementsByTagName('img'))
+        elements[i].style.width = "100%";
+        elements[i].style.display = "flex";
+        elements[i].style.flexDirection = "column";
+        elements[i].style.justifyContent = "center";
+        elements[i].style.alignItems = "center";
+        elements[i].getElementsByTagName('img')[0].style.width = "400px";
+        elements[i].getElementsByTagName('img')[0].style.height = "300px";
+    }
 }
 
 // Grid View
 function gridView() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "50%";
-  }
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.width = "50%";
+        elements[i].getElementsByTagName('img')[0].style.width = "300px";
+        elements[i].getElementsByTagName('img')[0].style.height = "250px";
+    }
 }
 
 rowImagenes = () => {
     let lista = [
         {
-            id : '1',
-            titulo : 'Columna',
-            descripcion : 'Descripcion prueba',
-            imagen : 'https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg'
+            id: '1',
+            titulo: 'Columna',
+            descripcion: 'Descripcion prueba',
+            imagen: 'https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg'
         },
         {
-            id : '2',
-            titulo : 'Columna',
-            descripcion : 'Descripcion prueba',
-            imagen : 'https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg'
+            id: '2',
+            titulo: 'Columna',
+            descripcion: 'Descripcion prueba',
+            imagen: 'https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg'
         },
         {
-            id : '3',
-            titulo : 'Columna',
-            descripcion : 'Descripcion prueba',
-            imagen : 'https://vignette.wikia.nocookie.net/infotic/images/d/d9/Usuarios.png/revision/latest?cb=20170827062729&path-prefix=es'
+            id: '3',
+            titulo: 'Columna',
+            descripcion: 'Descripcion prueba',
+            imagen: 'https://vignette.wikia.nocookie.net/infotic/images/d/d9/Usuarios.png/revision/latest?cb=20170827062729&path-prefix=es'
         },
         {
-            id : '4',
-            titulo : 'Columna',
-            descripcion : 'Esta imagen esta probada',
-            imagen : 'https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg'
+            id: '4',
+            titulo: 'Columna',
+            descripcion: 'Esta imagen esta probada',
+            imagen: 'https://sistemas.com/termino/wp-content/uploads/Usuario-Icono.jpg'
         }
     ]
     let rowImagenes = document.getElementById('rowImagenes')
     rowImagenes.innerHTML = ""
     lista.map(item => {
-        console.log('ValorLista: ',item)
+        console.log('ValorLista: ', item)
         rowImagenes.innerHTML += ` 
         <div class="column" style="background-color:#aaa;">
             <img src=${item.imagen} class="imagenPublica">
